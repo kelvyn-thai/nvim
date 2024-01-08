da9757a27e39393c35da9cb0c9a502c8a998cb3c
jayce.thai <phat.tnh@carehealth.io>
2024-01-08 18:06:13 +0700

    Transparency terminal background

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -7,15 +7,13 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "onedark",
   theme_toggle = { "onedark", "one_light" },
-
+  transparency = true,
   hl_override = highlights.override,
   hl_add = highlights.add,
 }
 
-
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
-
 
 return M
